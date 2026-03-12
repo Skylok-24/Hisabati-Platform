e0ff8a2fb7258be95a891b98a168fd88c28477da
Lokman Abdelmoname <brahmia.lokmeneabdelmoname@univ-guelma.dz>
2026-03-12 12:44:28 +0100

    clean commit without env

--- trusthandle/settings.py
+++ trusthandle/settings.py
@@ -152,10 +152,12 @@ REDIS_CLIENT = redis.from_url(
     decode_responses=True
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
@@ -169,3 +171,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+DEFAULT_FROM_EMAIL = "RafeeTech <brahmialokman16@gmail.com>"
